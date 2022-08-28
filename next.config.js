a215bd37c5703015952969ef1de4ffb7f8263eda
Kashif Siddique <61106834+mkashifsiddiqu@users.noreply.github.com>
2022-08-28 10:44:16 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Login',
+        permanent: true,
+      },
+    ]
+  },
+  reactStrictMode: true,
+  typescript:{
+    ignoreBuildErrors:true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+    
+  } 
+};
